155c081b696ee736bd8eae604f8958ccfcfd0dcb
Chris Ryan <c.ryan@c02yx3uvlvdr.springernature.com>
2019-12-31 13:15:01 +0000

    Sorts journals alphabetical

--- src/configuration/templates/main.js
+++ src/configuration/templates/main.js
@@ -2,6 +2,23 @@ const journals = require("../journals")
 
 const templates = {}
 
+// Sorth the journals into alphabetical order
+journals.sort((a, b) => {
+	const nameA = a.name.toUpperCase()
+	const nameB = b.name.toUpperCase()
+
+	if (nameA < nameB) {
+		return -1
+	}
+
+	if (nameA > nameB) {
+		return 1
+	}
+
+	// names must be equal
+	return 0
+})
+
 journals.forEach(item => {
 	templates[item.name] = {
 		card: {
